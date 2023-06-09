524ebd390f51487356022db1bbd477bff561b1eb
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-06-09 14:28:04 -0700

    basic black home page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,13 +4,10 @@ function Navbar() {
   const dotCss: string = `px-0.5 text-black`
   return (
     <nav className="w-full justify-end flex">
-      <ul className="mr-8 flex list-none font-semibold text-4xl">
+      <ul className="space-x-4 flex list-none font-semibold text-3xl">
         <li>HOME</li>
-        <li className={dotCss}>.</li>
         <li>WORK</li>
-        <li className={dotCss}>.</li>
         <li>ABOUT</li>
-        <li className={dotCss}>.</li>
         <li>CONTACT</li>
       </ul>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,28 @@ import Navbar from "@/components/navbar";
 
 function Home() {
   return (
-    <main className="min-h-screen justify-between flex flex-col w-full bg-black text-white">
+    <main className="relative min-h-screen justify-between flex flex-col w-full bg-bg-dark text-white">
       <Head>Bridger Brown Dev</Head>
-      <div className="m-4 w-full flex justify-end">
-        <Navbar />
+
+      <div className="absolute flex justify-center items-center w-full min-h-screen">
+        <div className="bg-bg-dark border-[#dbdbdb] border-2 w-[95.5vw] min-h-[95.5vh]">
+        </div>
       </div>
-      <div className="m-4 w-full flex items-end justify-start">
+
+      <div className="pt-5 pr-7 w-full z-10 flex justify-end">
+        <div className="pl-2 pb-1 bg-bg-dark w-fit">
+        <Navbar />
+        </div>
+      </div>
+
+      <div className="w-fit bg-bg-dark z-10 pl-7 pr-2 pt-1 pb-5 flex items-end justify-start">
         <h2 className="text-2xl">
         </h2>
-        <h1 className="text-6xl font-bold text-white">
+        <h1 className="text-8xl font-bold text-white">
           BRIDGER BROWN  
         </h1>
       </div>
+
     </main>
   );
 }
