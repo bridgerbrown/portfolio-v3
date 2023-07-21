507b1a4c9ba6dec82c99146b826bb4580240abe3
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-20 18:24:51 -0700

    Added Daynotes project

--- components/Project.tsx
+++ components/Project.tsx
@@ -8,7 +8,7 @@ function Project(props: any){
 
   return (
     <section className="w-full max-w-[1300px] flex flex-col"> 
-      <div className="cursor-pointer opacity-90 transition hover:opacity-100 hover:scale-102 w-full lg:h-fit xl:h-[275px] border-white-1 flex lg:flex-row flex-col items-center lg:items-start mb-4">
+      <div className="cursor-pointer transition hover:scale-102 w-full lg:h-fit xl:h-[275px] border-white-1 flex lg:flex-row flex-col items-center lg:items-start mb-4">
         <div className="rounded-sm w-11/12 sm:w-2/3 lg:w-1/3 min-w-[300px] max-w-none lg:max-w-sm bg-white">
           <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
             <Image
