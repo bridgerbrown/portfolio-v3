16929a60ab09fb13ae88145212f2f8072a0112b0
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-20 20:01:41 -0700

    home responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import Head from "next/head";
 import HomeNavbar from "@/components/HomeNavbar";
-import AnimatedPage from "@/components/AnimatedPage"; 
+import AnimatedPage from "@/components/AnimatedPage";
 
 function Home() {
 
@@ -13,22 +13,22 @@ function Home() {
         >
           <Head>Bridger Brown Dev</Head>
 
-          <div className="fixed z-10 left-homeBoxPosition right-homeBoxPosition top-homeBoxPosition bottom-homeBoxPosition">
+          <div className="fixed z-10 bg-nearBlack left-homeBoxPosition right-homeBoxPosition top-homeBoxPosition bottom-homeBoxPosition">
             <div className="w-[100%] h-[1px] left-0 top-0 absolute bg-white opacity-50"></div>
             <div className="w-[1px] h-[100%] left-0 top-0 absolute bg-white opacity-50"></div>
             <div className="w-[100%] h-[1px] left-0 bottom-0 absolute bg-white opacity-50"></div>
             <div className="w-[1px] h-[100%] right-0 top-0 absolute bg-white opacity-50"></div>
           </div>
 
-          <div className="pt-10 pr-12 w-full z-10 flex justify-end">
+          <div className="pt-8 pr-10 sm:pr-12 w-full z-10 flex justify-end">
               <HomeNavbar />
           </div>
 
-          <header className="absolute bottom-homeBoxPosition w-fit z-10 pl-12 pr-2 pb-4 flex items-end justify-start">
-            <h1 className="transition-all cursor-pointer text-8xl font-thin text-white">
+          <header className="absolute bottom-homeBoxPosition w-fit z-10 pl-10 lg:pl-12 pr-2 pb-4 flex md:flex-row flex-col md:items-end items-start justify-start">
+            <h1 className="transition-all cursor-pointer text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-thin text-white">
               BRIDGER BROWN  
             </h1>
-            <h2 className="cursor-pointer tracking-wide pl-4 text-2xl font-thin text-white">
+            <h2 className="cursor-pointer tracking-wide pl-0 md:pl-4 text-md sm:text-2xl font-thin text-white">
               Frontend Developer
             </h2>
           </header>
