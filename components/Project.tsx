be104b7b28bdd7ce9ee039e3b9287515901f2026
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-11-13 14:21:57 -0800

    implemented disabled project buttons logic

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,8 +3,33 @@ import React from "react";
 
 function Project(props: any){
   const buttonCSS: string = `flex justify-center items-center rounded-sm text-sm font-normal w-20 mr-2 sm:mr-6 hover:text-black h-10 bg-gray-300 text-black hover:bg-gray-400 hover:border-none border border-white-1 hover:text-white transition-colors`;
+  const disabledButtonCSS: string = `bg-gray-500 text-gray-800 flex justify-center items-center rounded-sm text-sm font-normal w-20 mr-2 sm:mr-6 h-10`;
   const { project } = props; 
   const builtWith = project.builtWith;
+
+  function disableButtonStyle(type: string): string {
+    if (project.buttonsEnabled === "11") {
+      return buttonCSS;
+    } else if (project.buttonsEnabled === "01") {
+      return type === "view" ? disabledButtonCSS : buttonCSS;
+    } else if (project.buttonsEnabled === "10") {
+      return type === "github" ? disabledButtonCSS : buttonCSS;
+    } else {
+      return disabledButtonCSS;
+    };
+  };
+
+  function disableButtons(type: string): string {
+    if (project.buttonsEnabled === "11") {
+      return type === "view" ? project.projectLink : project.githubLink;
+    } else if (project.buttonsEnabled === "01") {
+      return type === "view" ? "#" : project.githubLink;
+    } else if (project.buttonsEnabled === "10") {
+      return type === "github" ? "#" : project.projectLink;
+    } else {
+      return "#";
+    };
+  };
 
   return (
     <section className="tracking-wide w-full max-w-[1300px] flex flex-col"> 
@@ -12,7 +37,7 @@ function Project(props: any){
         className="cursor-pointer transition hover:scale-102 w-full lg:h-fit xl:h-[275px] border-white-1 flex lg:flex-row flex-col items-center lg:items-start mb-4"
       >
         <div className="rounded-sm w-11/12 sm:w-2/3 lg:w-1/3 min-w-[300px] max-w-none lg:max-w-sm bg-white">
-          <a href={project.projectLink} rel="noopener noreferrer">
+          <a href={disableButtons("view")} rel="noopener noreferrer">
             <Image
               src={project.img}
               alt="Project image"
@@ -62,10 +87,17 @@ function Project(props: any){
             </p>
           </div>
           <div className="mt-8 sm:mt-6 xl:mt-0 justify-end items-end flex">
-            <a className={buttonCSS} href={project.projectLink} rel="noopener noreferrer">
+            <a 
+              className={disableButtonStyle("view")} 
+              href={disableButtons("view")} 
+              rel="noopener noreferrer">
               View
             </a>
-            <a className={buttonCSS} href={project.githubLink} rel="noopener noreferrer">
+            <a 
+              className={disableButtonStyle("github")} 
+              href={disableButtons("github")} 
+              rel="noopener noreferrer"
+            >
               Github 
             </a>
           </div>
