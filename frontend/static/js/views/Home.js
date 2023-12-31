0a0dfc8248397e66aefaaba2484d3f5ae0329eb7
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-12-30 18:41:51 -0800

    home nave complete

--- frontend/static/js/views/Home.js
+++ frontend/static/js/views/Home.js
@@ -9,13 +9,19 @@ export default class extends AbstractView {
     return `
       <div id="page">
         <main id="home__container">
+          <div id="nav__home-container">
+            <nav id="nav__home">
+              <a href="/" class="nav__link" data-link>Home</a>
+              <a href="/projects" class="nav__link" data-link>Projects</a>
+              <a href="/about" class="nav__link" data-link>About</a>
+              <a href="/contact" class="nav__link" data-link>Contact</a>
+            </nav>
+          </div>
           <div id="home__linebox">
             <div class="home__linebox-line" id="home__linebox-top"></div>
             <div class="home__linebox-line" id="home__linebox-right"></div>
             <div class="home__linebox-line" id="home__linebox-bottom"></div>
             <div class="home__linebox-line" id="home__linebox-left"></div>
-          </div>
-          <div class="nav__container-home">
           </div>
           <header class="home__heading-container">
             <h1>
