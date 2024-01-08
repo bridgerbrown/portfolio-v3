a7fdeb2c9b120145f0a2f6c2943245317999601b
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2024-01-08 00:36:45 -0800

    solved the commonjs problem, going back to static

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename)
+console.log(__dirname)
 
 const app = express();
 
