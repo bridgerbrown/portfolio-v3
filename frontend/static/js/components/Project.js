0ed4c58a3429588096fb55185ba8236587df60d5
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2024-01-08 21:00:03 -0800

    trying url path fix one more time

--- frontend/static/js/components/Project.js
+++ frontend/static/js/components/Project.js
@@ -7,8 +7,8 @@ export default class Project extends HTMLElement {
 
     const project = JSON.parse(this.dataset.project);
     const projectImg = this.querySelector("img");
-    const imgPath = `/static/public${project.img}`;
-    projectImg.src = new URL(imgPath, import.meta.url)
+    const imgPath = `./static/public${project.img}`;
+    projectImg.src = new URL(imgPath, import.meta.url).toString();
     projectImg.title = project.title + " image preview"
     projectImg.alt = project.title + " image preview"
     this.querySelector("h2").textContent = project.title;
@@ -26,8 +26,8 @@ export default class Project extends HTMLElement {
     const headingTop = this.querySelector(".project__heading-top");
     const isFeatured = project.featured === "true";
     const featuredStar = document.createElement("img");
-    const starPath = "/static/public/star.png";
-    featuredStar.src = new URL(starPath, import.meta.url)
+    const starPath = "./static/public/star.png";
+    featuredStar.src = new URL(starPath, import.meta.url).toString();
     featuredStar.alt = "Featured project icon, a yellow star";
     featuredStar.id = "project__featured-icon";
     if (isFeatured) headingTop.appendChild(featuredStar)
