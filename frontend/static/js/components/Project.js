9fd2ec42898baec139c47b229ba2be52d0865d3c
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2024-01-08 20:48:51 -0800

    had to use url dependencies for project images

--- frontend/static/js/components/Project.js
+++ frontend/static/js/components/Project.js
@@ -7,7 +7,8 @@ export default class Project extends HTMLElement {
 
     const project = JSON.parse(this.dataset.project);
     const projectImg = this.querySelector("img");
-    projectImg.src = `/static/public${project.img}`;
+    const imgPath = `/static/public${project.img}`;
+    projectImg.src = new URL(imgPath, import.meta.url)
     projectImg.title = project.title + " image preview"
     projectImg.alt = project.title + " image preview"
     this.querySelector("h2").textContent = project.title;
@@ -25,7 +26,8 @@ export default class Project extends HTMLElement {
     const headingTop = this.querySelector(".project__heading-top");
     const isFeatured = project.featured === "true";
     const featuredStar = document.createElement("img");
-    featuredStar.src = "/static/public/star.png";
+    const starPath = "/static/public/star.png";
+    featuredStar.src = new URL(starPath, import.meta.url)
     featuredStar.alt = "Featured project icon, a yellow star";
     featuredStar.id = "project__featured-icon";
     if (isFeatured) headingTop.appendChild(featuredStar)
